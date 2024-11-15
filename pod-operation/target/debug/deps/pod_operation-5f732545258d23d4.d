264c8e8bc21a6c2df2279e9c64f7a6e06270b34d
Jason Kim <73210729+kimjason832@users.noreply.github.com>
2024-11-14 17:16:11 -0800

    Revert "Axum setup"

---
+++ pod-operation/target/debug/deps/pod_operation-5f732545258d23d4.d
@@ -0,0 +1,5 @@
+/home/vrushanganand/Desktop/Projects/hx10-control-system/pod-operation/target/debug/deps/libpod_operation-5f732545258d23d4.rmeta: src/main.rs
+
+/home/vrushanganand/Desktop/Projects/hx10-control-system/pod-operation/target/debug/deps/pod_operation-5f732545258d23d4.d: src/main.rs
+
+src/main.rs:
