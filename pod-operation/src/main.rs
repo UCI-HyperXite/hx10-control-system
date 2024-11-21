ee18a898a89690b6b7a424363b2355e3149ead1d
Vrushang Anand <103944547+vrushang1234@users.noreply.github.com>
2024-11-21 10:19:14 -0800

    Rust formatter (#8)
    * Added formatter and lint checker for rust
    
    * Added gitignore
    
    * Fixed merge conflicts

--- pod-operation/src/main.rs
+++ pod-operation/src/main.rs
@@ -3,15 +3,15 @@ use socketioxide::SocketIo;
 
 #[tokio::main]
 async fn main() {
-    // Make a layer for Socket connection
-    let (layer, _io) = SocketIo::new_layer();
+	// Make a layer for Socket connection
+	let (layer, _io) = SocketIo::new_layer();
 
-    // Initialize the
-    let app: Router = Router::new()
-        .route("/", get(|| async { "Hello world" }))
-        .layer(layer);
+	// Initialize the
+	let app: Router = Router::new()
+		.route("/", get(|| async { "Hello world" }))
+		.layer(layer);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
+	let listener = tokio::net::TcpListener::bind("0.0.0.0:5000").await.unwrap();
 
-    axum::serve(listener, app).await.unwrap();
+	axum::serve(listener, app).await.unwrap();
 }
