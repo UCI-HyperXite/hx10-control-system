d808e6dfaf146bd3ba9acaa2ee5de3209665f3d7
Daphne <daphne922003@gmail.com>
2025-02-03 20:16:30 -0800

    Add overlay design to GUI

--- control-station/src/Components/mainBody/sensorsContainer/sensorBoxes/sensorBox.tsx
+++ control-station/src/Components/mainBody/sensorsContainer/sensorBoxes/sensorBox.tsx
@@ -4,12 +4,13 @@ import Speedometer from "./speedometer/speedometer";
 interface propInterface {
   name: string;
   value: number;
+  onClick:  () => void; //handling overlay
 }
 
 export default function SensorBox(prop: propInterface) {
   return (
-    <div className="sensorBox">
-      <h2>{prop.name}</h2>
+    <div className="sensorBox" onClick={prop.onClick}> {/* for overlay */}
+      <h2>{prop.name}</h2> 
       {prop.name === "Speed" ? (
         <Speedometer value={prop.value} /> // Render Speedometer if the name is Speed
       ) : (
