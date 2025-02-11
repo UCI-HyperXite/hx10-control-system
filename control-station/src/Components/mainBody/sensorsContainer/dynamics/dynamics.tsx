5d12482aa5b4737c3992cc2c9195fe048c6c9400
Daphne <daphne922003@gmail.com>
2025-02-10 21:27:40 -0800

    Using dynamic data and updating status indicator

--- control-station/src/Components/mainBody/sensorsContainer/dynamics/dynamics.tsx
+++ control-station/src/Components/mainBody/sensorsContainer/dynamics/dynamics.tsx
@@ -2,23 +2,47 @@ import "./dynamics.css";
 import FrontPod from "@/assets/images/FrontPod 1.png";
 import TopPod from "@/assets/images/TopPod.png";
 import SidePod from "@/assets/images/SidePod 1.png";
+
+import { useState } from "react";
+import { MOCK_POD_DATA } from "@/Services/PodSocketClient";
+import { PodData } from "@/Services/PodSocketClient";
+
 export default function Dynamics() {
+  const [podData, setPodData] = useState<PodData>(MOCK_POD_DATA);
   return (
     <div className="dynamics-container">
       <div className="dynamics-data">
         <h2>Roll</h2>
         <br />
-        <img src={FrontPod} />
+        <img
+          src={FrontPod}
+          alt="front view of the pod"
+          style={{
+            transform: `rotate(${Math.round(podData.gyroscope.roll * 100) / 100}deg)`,
+          }}
+        />
       </div>
       <div className="dynamics-data">
         <h2>Pitch</h2>
         <br />
-        <img src={SidePod} />
+        <img
+          src={SidePod}
+          alt="front view of the pod"
+          style={{
+            transform: `rotate(${Math.round(podData.gyroscope.pitch * 100) / 100}deg)`,
+          }}
+        />
       </div>
       <div className="dynamics-data">
         <h2>Yaw</h2>
         <br />
-        <img src={TopPod} />
+        <img
+          src={TopPod}
+          alt="front view of the pod"
+          style={{
+            transform: `rotate(${Math.round(podData.gyroscope.yaw * 100) / 100}deg)`,
+          }}
+        />
       </div>
     </div>
   );
