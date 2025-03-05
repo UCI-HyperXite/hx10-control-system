317a5194635fd2cb9e5e67b47cec42c649a6454e
Jason Kim <kimjason.832@gmail.com>
2025-03-05 11:15:04 -0800

    initial components and gpios

---
+++ pod-operation/include/components.h
@@ -0,0 +1,18 @@
+#include "components/c/brakes.h"
+#include "components/c/gpio.h"
+#include "components/c/lim_current.h"
+#include "components/c/lim_temperature.h"
+#include "components/c/pico_relay.h"
+#include "components/c/pressure_transducer.h"
+#include "components/c/signal_light.h"
+#include "components/c/wheel_encoder.h"
+#include "components/c/yaw.h"
+
+#include "components/cpp/gyro.hpp"
+#include "components/cpp/high_voltage_system.hpp"
+#include "components/cpp/inverter_board.hpp"
+#include "components/cpp/lidar.hpp"
+// #include "components/cpp/motors.hpp"
+
+
+
