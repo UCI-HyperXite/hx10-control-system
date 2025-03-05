d856954f59f2157bb71bdbe32c5b2f83ff37e86b
Jason Kim <kimjason.832@gmail.com>
2025-03-05 12:51:10 -0800

    nitial components and gpios

---
+++ pod-operation/include/state_machine.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+typedef bool(*FSM_EVENT)(void);
+typedef void(*FSM_ACTION)(void);
+
+typedef enum {
+    INIT, LOAD, PRECHARGE, START, STOP, FAULT, HALT
+} FSM_STATE;
---
+++ pod-operation/src/utils/gpio.c
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <cassert>
+// #include <wiringPi.h>
+// #include "pod-operation/include/utils/gpio.h"
+
+void initailizeGPIOs(void){
+    wiringPiSetup();
+
+    // pinMode(I2C_SDA, );
+    // pinMode(I2C_SCL, );
+
+    pinMode(VL53L0X_FRONT, INPUT);
+    pinMode(VL53L0X_BACK, INPUT);
+    pinMode(WHEEL_ENCODER_A, INPUT);
+    pinMode(WHEEL_ENCODER_B, INPUT);
+    pinMode(VL6180X, INPUT);
+    pinMode(CONTACTOR_RELAY, INPUT);
+    pinMode(SIGNAL_LIGHT_RELAY, INPUT);
+    pinMode(PNEUMATICS_RELAY, INPUT);
+}
+
+void setHigh(int pin){
+    digitalWrite(pin, HIGH);
+}
+
+void setLow(int pin){
+    digitalWrite(pin, LOW);
+}
