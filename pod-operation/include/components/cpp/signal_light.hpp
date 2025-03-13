8339ecb5c4861242265c375ad8ff4097786395ac
Jason Kim <kimjason.832@gmail.com>
2025-03-12 18:48:46 -0700

    added signal light code

---
+++ pod-operation/include/components/cpp/signal_light.hpp
@@ -0,0 +1,23 @@
+#ifndef SIGNALLIGHT_HPP
+#define SIGNALLIGHT_HPP
+
+#include <wiringPi.h>
+#include <iostream>
+#include <sys/time.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#define LED_PIN 1  // WiringPi pin 1 (GPIO18)
+#define T1H 680    // Ideal time 800
+#define T1L 310    // Ideal time 450
+#define T0H 300    // Ideal time 400
+#define T0L 750    // Ideal time 850
+#define TCOMP 5000 // RP1 wake up time in nanoseconds
+#define RESET_TIME 50  // Reset time in microseconds
+#define LED_NUMBER 150  // Number of LEDs
+#define RAINBOW_DELAY 2000000 
+
+uint32_t color_matrix[LED_NUMBER];
+uint32_t colorWheel(uint8_t pos);
+
+#endif
---
+++ pod-operation/src/components/cpp/signal_light.cpp
@@ -0,0 +1,118 @@
+#include "../../include/components/cpp/signal_light.hpp"
+
+class Timer {
+public:
+    void start() {
+        clock_gettime(CLOCK_MONOTONIC, &start_time);
+    }
+
+    long elapsed() {
+        struct timespec end_time;
+        clock_gettime(CLOCK_MONOTONIC, &end_time);
+        return (end_time.tv_sec - start_time.tv_sec) * 1e9 + (end_time.tv_nsec - start_time.tv_nsec);
+    }
+private:
+    struct timespec start_time;
+};
+
+Timer timer;
+void setRed(uint32_t color_matrix[LED_NUMBER], int i);
+void setYellow(uint32_t color_matrix[LED_NUMBER], int i);
+void setGreen(uint32_t color_matrix[LED_NUMBER], int i);
+void setYellowBlink(uint32_t color_matrix[LED_NUMBER]);
+void setGreenBlink(uint32_t color_matrix[LED_NUMBER]);
+void sendBit(bool bit);
+void sendColors(uint32_t* colors);
+uint32_t colorWheel(uint8_t pos);
+
+
+int main() {
+    // Initialize WiringPi and set the pin mode
+    if (wiringPiSetup() == -1) {
+        std::cerr << "WiringPi setup failed!" << std::endl;
+        return 1;
+    }
+    pinMode(LED_PIN, OUTPUT);
+
+    int flag = 0;
+    uint8_t colorPos = 0;
+    while (true) {
+        for (int i = 0; i < LED_NUMBER; i++) {
+            setRed(color_matrix, i);
+        }
+        sendColors(color_matrix);
+        usleep(RAINBOW_DELAY);
+        for (int i = 0; i < LED_NUMBER; i++) {
+            setYellow(color_matrix, i);
+        }
+        sendColors(color_matrix);
+        usleep(RAINBOW_DELAY);
+        // for (int i = 0; i < LED_NUMBER; i++) {
+        //     color_matrix[i] = 0x000000; 
+        // }
+        // sendColors(color_matrix);
+        // usleep(RAINBOW_DELAY);
+        
+
+        for (int i = 0; i < LED_NUMBER; i++) {
+            setGreen(color_matrix, i);
+        }
+        sendColors(color_matrix);
+        usleep(RAINBOW_DELAY);
+    }
+    return 0;
+}
+
+
+void setRed(uint32_t color_matrix[LED_NUMBER], int i){
+    // color_matrix[i] = 0x00FF00;
+    color_matrix[i] = 0x002000;
+}
+void setYellow(uint32_t color_matrix[LED_NUMBER], int i){
+    // color_matrix[i] = 0xFFFF00;
+    color_matrix[i] = 0x202000;
+}
+void setGreen(uint32_t color_matrix[LED_NUMBER], int i){
+    // color_matrix[i] = 0xFF0000;
+    color_matrix[i] = 0x200000;
+}
+
+
+
+
+void sendBit(bool bit) {
+    if (bit) {
+        // Send '1'
+        digitalWrite(LED_PIN, HIGH);
+        timer.start();
+        while (timer.elapsed() < T1H); // High time for '1'
+        digitalWrite(LED_PIN, LOW);
+        timer.start();
+        while (timer.elapsed() < T1L); // Low time for '1'
+    } else {
+        // Send '0'
+        digitalWrite(LED_PIN, HIGH);
+        timer.start();
+        while (timer.elapsed() < T0H); // High time for '0'
+        digitalWrite(LED_PIN, LOW);
+        timer.start();
+        while (timer.elapsed() < T0L); // Low time for '0'
+    }
+}
+
+
+void sendColors(uint32_t *colors) {
+    digitalWrite(LED_PIN, HIGH);     // Changed the way WAKEUP occurs. Now a single 20ns pulse get generated, then we wait. 
+    digitalWrite(LED_PIN, LOW);	     //  This pulse is so fast it gets ignored by the WS2812B and ensure accurate timings.
+    timer.start();
+    while (timer.elapsed() < TCOMP); // Wait for the RP1 to wake up before sending data
+
+    for (int j = 0; j < LED_NUMBER; j++) {
+        for (int i = 23; i >= 0; i--) {
+            sendBit((colors[j] >> i) & 1);
+        }
+    }
+
+    // Ensure reset time
+    usleep(RESET_TIME);
+}
