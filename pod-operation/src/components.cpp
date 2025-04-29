31c848abc6baafaf90fd058019151bde523252c0
Jason Kim <kimjason.832@gmail.com>
2025-04-28 19:54:55 -0700

    revised code after dynamics testing

--- pod-operation/src/components.cpp
+++ pod-operation/src/components.cpp
@@ -24,7 +24,7 @@ void readGyro(MPU6050* gyro)
 
 void readPodHeight(vl6180* pod_height) {
     while (true) {
-        int distance = get_distance(*pod_height);
+        int distance = get_distance(*pod_height) + 10;
         std::cout << "[VL6180X] Pod Height: " << distance << " mm" << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
